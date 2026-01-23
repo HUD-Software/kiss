a4bef417bde926b89d1145971c582f8a0129dfb0
Julian <hudsoftware@outlook.fr>
2026-01-23 10:40:11 +0100

    Improve target

--- src/target/__init__.py
+++ src/target/__init__.py
@@ -3,10 +3,20 @@ from pathlib import Path
 from typing import Optional, Self
 import yaml
 import console
+from project import ProjectType
+
+FeatureName = str
+ConfigName = str
+CompilerFlag = str
+LinkerFlag = str
 
 class TargetFeature:
-    def __init__(self):
-        pass
+    def __init__(self, name:FeatureName):
+        self._name = name
+    
+    @property
+    def name(self) -> FeatureName:
+        return self._name
     
     @staticmethod
     def from_yaml(yaml : dict) -> Self:
@@ -15,95 +25,221 @@ class TargetFeature:
             #console.print_error(f"⚠️  Error: Project name is missing in {self.file} under '{project_type_key}'")
             exit(1)
 
-
-class TargetCXXCompiler:
+class TargetCXXCompilerNode:
     def __init__(self):
-        self._all_config_default_feature_names : list[str]= []
-        self._config_feature_names : dict[str, list[str]] = dict[str, list[str]]()
+        self._default_features : dict[ConfigName, list[FeatureName]] = dict[ConfigName, list[FeatureName]]()
+        self._flags : dict[ConfigName, list[CompilerFlag]] = dict[ConfigName, list[CompilerFlag]]()
         
     @property
-    def all_config_feature_names(self):
-        return self._all_config_default_feature_names
+    def default_features(self) -> dict[ConfigName, list[FeatureName]]:
+        return self._default_features
     
-    def config_feature_names(self, config: str) -> list[str]:
-        return self._config_feature_names.setdefault(config, [])
+    @property
+    def flags(self) -> dict[ConfigName, list[CompilerFlag]]:
+        return self._flags
     
-    
-    
+    @staticmethod
+    def from_yaml(node_name: str, yaml : dict) -> Self:
+        cxx_compiler_node = TargetCXXCompilerNode()
+        for item, yaml_object in yaml.items():
+            match item:
+                case  "default-features":
+                    for config_name, yaml_object in yaml_object.items():
+                            cxx_compiler_node.default_features.setdefault(config_name, []).extend(yaml_object)
+                case _:
+                    parts = item.split('-')
+                    if len(parts) != 2:
+                        console.print_error(f"Invalid '{item}' under '{node_name}' target")
+                        return None
+                    config_name = parts[0]
+                    if parts[1] == "flags":
+                        cxx_compiler_node.flags.setdefault(config_name,[]).extend(yaml_object)
+                    else:
+                        console.print_error(f"Invalid '{parts[1]}' in '{item}' item under '{node_name}' target")
+        return cxx_compiler_node
 
-class TargetCXXLinker:
+class TargetCXXLinkerNode:
     def __init__(self):
-        pass
+        self._default_features : dict[ConfigName, list[FeatureName]] = dict[ConfigName, list[FeatureName]]()
+        self._flags : dict[ConfigName, list[LinkerFlag]] = dict[ConfigName, list[LinkerFlag]]()
+        
+    @property
+    def default_features(self) -> dict[ConfigName, list[FeatureName]]:
+        return self._default_features
     
+    @property
+    def flags(self) -> dict[ConfigName, list[LinkerFlag]]:
+        return self._flags
 
-class Target:
-    def __init__(self, file: Path, name: str):
-        self._file = file
+    @staticmethod
+    def from_yaml(node_name: str, yaml : dict) -> Self:
+        cxx_linker_node = TargetCXXLinkerNode()
+        for item, yaml_object in yaml.items():
+            match item:
+                case  "default-features":
+                    for config_name, yaml_object in yaml_object.items():
+                            cxx_linker_node.default_features.setdefault(config_name, []).extend(yaml_object)
+                case _:
+                    parts = item.split('-')
+                    if len(parts) != 2:
+                        console.print_error(f"Invalid '{item}' under '{node_name}' target")
+                        return None
+                    config_name = parts[0]
+                    if parts[1] == "flags":
+                        cxx_linker_node.flags.setdefault(config_name,[]).extend(yaml_object)
+                    else:
+                        console.print_error(f"Invalid '{parts[1]}' in '{item}' item under '{node_name}' target")
+        return cxx_linker_node       
+    
+class TargetProjectTypeNode:
+    def __init__(self, name:str):
         self._name = name
-        
-    @property
-    def file(self):
-        return self._file
-
+        self._cxx_compiler: Optional[TargetCXXCompilerNode] = None
+        self._cxx_linker: Optional[TargetCXXLinkerNode] = None
+    
     @property
     def name(self):
         return self._name
+    
+    @property
+    def cxx_compiler(self):
+        return self._cxx_compiler
+   
+    @cxx_compiler.setter
+    def cxx_compiler(self, value: TargetCXXCompilerNode):
+        self._cxx_compiler = value
 
+    @property
+    def cxx_linker(self):
+        return self._cxx_linker
+   
+    @cxx_linker.setter
+    def cxx_linker(self, value: TargetCXXCompilerNode):
+        self._cxx_linker = value
+
+    @staticmethod
+    def from_yaml(node_name: str, yaml : dict) -> Self:
+        project_type_node = TargetProjectTypeNode(node_name)
+        for item, yaml_object in yaml.items():
+            match item:
+                case "cxx-compiler":
+                    if project_type_node.cxx_compiler:
+                        console.print_error(f"Multiple cxx-compiler in '{node_name}' is not supported")
+                    project_type_node.cxx_compiler= TargetCXXCompilerNode.from_yaml(node_name=node_name, yaml=yaml_object)
+                case "cxx-linker":
+                    if project_type_node.cxx_linker:
+                        console.print_error(f"Multiple cxx-linker in '{node_name}' is not supported")
+                    project_type_node.cxx_linker = TargetCXXLinkerNode.from_yaml(node_name=node_name, yaml=yaml_object)
+        return project_type_node  
+    
+class TargetNode:
+    def __init__(self, name: str, arch: str, os :str, env:str, vendor:str, compiler_name:str):
+        self._name = name
+        self._arch = arch
+        self._os = os
+        self._env = env
+        self._vendor = vendor
+        self._compiler_name = compiler_name
+        self._cxx_compiler: Optional[TargetCXXCompilerNode] = None
+        self._cxx_linker: Optional[TargetCXXLinkerNode] = None
+        self._project_type: Optional[TargetProjectTypeNode] = None
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def cxx_compiler(self) -> TargetCXXCompilerNode:
+        return self._cxx_compiler
+   
+    @cxx_compiler.setter
+    def cxx_compiler(self, value: TargetCXXCompilerNode):
+        self._cxx_compiler = value
+
+    @property
+    def cxx_linker(self) -> TargetCXXCompilerNode:
+        return self._cxx_linker
+   
+    @cxx_linker.setter
+    def cxx_linker(self, value: TargetCXXCompilerNode):
+        self._cxx_linker = value
+
+    @property
+    def project_type(self) -> TargetProjectTypeNode:
+        return self._project_type
+   
+    @project_type.setter
+    def project_type(self, value: TargetProjectTypeNode):
+        self._project_type = value
+
+    def _read_cxx_compiler_node(self, yaml_cxx_compiler : dict) -> TargetCXXCompilerNode:
+        if self.cxx_compiler:
+            console.print_error(f"Multiple cxx-compiler in '{self.name}' is not supported")
+        return TargetCXXCompilerNode.from_yaml(node_name=self.name, yaml=yaml_cxx_compiler)
+    
+    def _read_cxx_linker_node(self, yaml_cxx_linker : dict) -> TargetCXXLinkerNode:
+        if self.cxx_linker:
+            console.print_error(f"Multiple cxx-linker in '{self.name}' is not supported")
+        return TargetCXXLinkerNode.from_yaml(node_name=self.name, yaml=yaml_cxx_linker)
+   
+    def _read_project_type_node(self, node_name:str, yaml_project_type :dict) -> TargetProjectTypeNode:
+        if self.project_type:
+            console.print_error(f"Multiple 'project-type' in '{self.name}' is not supported")
+        return TargetProjectTypeNode.from_yaml(node_name=node_name, yaml=yaml_project_type)
+    
 
 class YamlTargetFile:
     def __init__(self, file: Path):
         self._file = file
     
-    def _load_cxx_compiler(self, yaml_cxx_compiler : dict) -> TargetCXXCompiler:
-        cxx_compiler = TargetCXXCompiler()
-        for item, yaml_object in yaml_cxx_compiler.items():
-            if item == "default-features":
-                for config_name, yaml_object in yaml_object.items():
-                    if config_name == "all":
-                        cxx_compiler.all_config_feature_names.extend(yaml_object)
-                    else:
-                        cxx_compiler.config_feature_names(config_name).extend(yaml_object)
-
-        return cxx_compiler
-    def _load_target(self, name: str, yaml_target : dict) -> Target:
+    def _read_target_node(self, name: str, yaml_target : dict) -> TargetNode:
         # Read the target name
         parts = name.split('-')
         if len(parts) != 5:
-            console.print_error(f"Invalid target name in {self._file}:\t\n'{name}'. Expected 'arch-vendor-os-env-compiler' separated by '-'")
+            console.print_error(f"Invalid target name in '{self._file}':\t\n'{name}'. Expected 'arch-vendor-os-env-compiler' separated by '-'")
             return None
-        # Common compiler target (name, Target)
-        all_compiler_target : dict[str, Target] = None
+        arch = parts[0]
+        vendor = parts[1]
+        os = parts[2]
+        env = parts[3]
+        compiler_name = parts[4]
+        target_node : TargetNode = TargetNode(name=name, arch=arch, os=os, env=env, vendor=vendor, compiler_name=compiler_name)
 
         for item, yaml_object in yaml_target.items():
-            if item == "cxx-compiler":
-                cxx_compiler : TargetCXXCompiler = self._load_cxx_compiler(yaml_cxx_compiler=yaml_object)
-                pass
-        # arch = parts[0]
-        # vendor = parts[1]
-        # os = parts[2]
-        # env = parts[3]
-        compiler = parts[4]
-        if compiler == "all":
-            pass
+            match item:
+                case "cxx-compiler":
+                    target_node._read_cxx_compiler_node(yaml_cxx_compiler=yaml_object)
+                case "cxx-linker":
+                    target_node._read_cxx_linker_node(yaml_cxx_linker=yaml_object)
+                case "features":
+                    break
+                case "include":
+                    break
+                case _:
+                    if item == ProjectType.bin or item == ProjectType.dyn or item == ProjectType.lib:
+                        target_node._read_project_type_node(node_name=item, yaml_project_type=yaml_object)
 
-    def load_yaml(self) -> list[Target] :
+        return target_node
+            
+
+    def load_yaml(self) -> list[TargetNode] :
         try:
             with self._file.open() as f:
                 self._yaml = yaml.safe_load(f)
                 for item, object in self._yaml.items():
-                    target = self._load_target(item, object)
+                    target = self._read_target_node(item, object)
                     if not target:
-                        console.print_error("target {item} will not be available")
+                        console.print_error(f"target '{item}' in '{self._file}' will not be available")
 
             return True
         except (OSError, yaml.YAMLError) as e:
-            console.print_error(f"Error: When loading {self._file} file: {e}")
+            console.print_error(f"Error: When loading '{self._file}' file: {e}")
             return False
         
 
 class TargetRegistry:
     def __init__(self):
-        self._targets: dict[str, Target] = {}
+        self._targets: dict[str, TargetNode] = {}
     
     def __contains__(self, name: str) -> bool:
         return name in self._targets
@@ -114,7 +250,7 @@ class TargetRegistry:
     def items(self):
         return self._targets.items()
     
-    def register_target(self, target: Target):
+    def register_target(self, target: TargetNode):
         existing_target = self._targets.get(target.name)
         if existing_target:
             console.print_error(f"⚠️  Warning: Project already registered: {existing_target.name} in {str(existing_target.file)}")
